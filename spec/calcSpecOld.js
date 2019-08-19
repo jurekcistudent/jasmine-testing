fda95f88cd77923210e6aaf21a41fa67c2e3b2f8
jurekcistudent <jurekoor@gmail.com>
2019-08-19 17:45:48 +0000

    Refactoring calcSpec.js

--- spec/calcSpecOld.js
+++ spec/calcSpecOld.js
@@ -1,23 +1,14 @@
 describe("Calculator", function() {
-
-    beforeEach(function() {
-        calc = new Calculator;
-    });
-
     describe("Addition tests", function() {
         it("should return 42", function() {
-            calc.add(20);
-            calc.add(22);
-            expect(calc.value).toBe(42)
+            expect(addition(20, 22)).toBe(42)
         });
         it("should return 26", function() {
-            calc.add(7);
-            calc.add(19);
-            expect(calc.value).toBe(26)
+            expect(addition(7, 19)).toBe(26)
         });
         it("should return a error if we don't supply two numbers", function() {
             spyOn(window, "alert");
-            calc.add("Hitchhikers");
+            addition("Hitchhikers", "Guide");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
